0bbe3ddf2fab458b96d9a6a20d005f4e2ae2d463
Nathan Chow <emailme@nathanchow.ca>
2021-05-25 20:07:34 -0400

    update

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <>
-    TEST
-    </>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
