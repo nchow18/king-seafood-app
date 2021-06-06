cc5a4ff752237f14afbdd92197aa177bf37c616d
Nathan Chow <emailme@nathanchow.ca>
2021-06-06 17:47:38 -0400

    update

--- client/src/components/Carousel/FeaturedProducts.js
+++ client/src/components/Carousel/FeaturedProducts.js
@@ -15,12 +15,15 @@ function FeaturedCarousel(props) {
   const [links] = useState({...products})
   const [currentProduct, setProduct] = useState(links[0])
 
-  const featuredProducts = products.filter(function (product) {
-    return product.product_featured === true;
-  })
-  
+  const featuredProducts = [];
 
-  if (products.length === 0) return `...Loading`;
+  for (var i = 0; i < products.length; i++) {
+    if (products[i].product_featured === true) {
+      featuredProducts.push(products[i])
+    }
+  }
+
+  if (featuredProducts.length === 0) return `...No Featured Products`;
 
   return (
   <>
