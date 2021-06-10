5080611f1b305251511659899c7106911cd4ded7
Nathan Chow <emailme@nathanchow.ca>
2021-06-10 01:59:20 -0400

    update promo page

--- client/src/pages/Promotions.js
+++ client/src/pages/Promotions.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Auth from '../utils/auth';
 import { PROMO, PRODUCTS } from '../utils/queries';
 import { useQuery } from '@apollo/react-hooks';
+import '../css/Promotions.css';
 
 function Promotions() {
     
@@ -14,40 +15,16 @@ function Promotions() {
 
     if (loading) return `...Loading`;
 
-    if (data) {
-        for (var i = 0; i < products.length; i++) {
-            if (promo[0].featuredProduct1 === products[i]._id) {
-                featuredProducts.push(products[i])
-            }
-            if (promo[0].featuredProduct2 === products[i]._id) {
-                featuredProducts.push(products[i])
-            }
-            if (promo[0].featuredProduct3 === products[i]._id) {
-                featuredProducts.push(products[i])
-            }
-        }
-    }
-
-    function viewProduct(id) {
-        Auth.setSingleProduct(id);
-        Auth.viewSingleProduct();
-    }
+    console.log(promo);
 
     return (
         <>
-    	    <div className="flex-c-column content">
-                <div className="flex-c-row">
-                    {featuredProducts.map((product) => (
-                        <div key={product._id} className="promo-img-container">
-                            <img alt={product.product_name} className="promo-img" src={product.product_picture} />
-                            <div key={product._id} onClick={() => { viewProduct( product._id )}} className="promo-img-title">
-                                <p>{product.product_name}</p>
-                                <p>RM {product.product_price}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>                
+    	    <div className="promo-page-content">
+            <div className="promo-page-container">
+              <img alt="promo" src={promo[0].promoPicture1} className="promo-page-img"/>
             </div>
+
+          </div>
         </>
     )
 }
