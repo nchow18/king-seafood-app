83f53e710556ef25e5aa019e54e681ac0a92ceea
Nathan Chow <emailme@nathanchow.ca>
2021-11-29 22:29:29 -0500

    update

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -11,13 +11,50 @@ function Products(props) {
 
   const categories = Auth.getCategories();
   const [productModal, setProductModal] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState();
+  const [imageCounter, setImageCounter] = useState(1);
+
+  function previousPic() {
+    if (imageCounter !== 1) {
+      setImageCounter(imageCounter - 1)
+    }
+  }
+
+  function nextPic() {
+    if (imageCounter !== currentProduct.product_picture.length) {
+      setImageCounter(imageCounter + 1)
+    }
+  }
 
   console.log(products);
+  console.log(currentProduct);
+  console.log(imageCounter);
 
   return (
     <>
       {productModal ? (
         <>
+          <div onClick={() => (setProductModal(false), setImageCounter(1))}><i className="fas fa-arrow-left font-size-12px"></i>   <b>Go Back</b></div>          
+          <div className="product-desktop-view">
+            <div className="product-desktop-image-container">
+              {currentProduct.product_picture.length > 1 && (
+                <>
+                  <i className="fas fa-arrow-left" onClick={() => (previousPic())}></i>              
+                </>
+              )}
+              <img alt={currentProduct._id} className="product-desktop-picture-full border-round" src={process.env.PUBLIC_URL + `/images/products/half_size/tn_${currentProduct.product_id}-${imageCounter}.jpg`} />
+              {currentProduct.product_picture.length > 1 && (
+                <>
+                  <i className="fas fa-arrow-right" onClick={() => (nextPic())}></i>            
+                </>
+              )}            
+
+            </div>
+            <div className="product-desktop-view-details">
+              <div>{currentProduct.product_name}</div>
+              <div>{currentProduct.product_price}</div>
+            </div>
+          </div>
         </>
       ) : (
         <div className="products-desktop-container">
@@ -33,12 +70,13 @@ function Products(props) {
           </div>
           <div className="products-desktop-list">
             {products.map((product) => (
-              <div key={product._id} className="product-desktop-card">
+              <div key={product._id} className="product-desktop-card" onClick={() => (setProductModal(true), setCurrentProduct(product))}>
                 <div className="display-flex-center-all">
-                  <img className="product-desktop-picture border-round" src={process.env.PUBLIC_URL + `/images/products/half_size/tn_${product.product_id}-1.jpg`} />
+                  <img alt={product._id} className="product-desktop-picture border-round" src={process.env.PUBLIC_URL + `/images/products/half_size/tn_${product.product_id}-1.jpg`} />
                 </div>
                 <div className="products-desktop-description">
-                  {product.product_name}
+                  <div><b>{product.product_name}</b></div>
+                  <div>RM {product.product_price}</div>
                 </div>
               </div>
             ))}
