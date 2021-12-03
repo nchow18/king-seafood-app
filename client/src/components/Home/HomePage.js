6f1a6b9ad260473b2a9d6790e636f74c6ab8ed84
Nathan Chow <emailme@nathanchow.ca>
2021-12-02 23:45:35 -0500

    update

--- client/src/components/Home/HomePage.js
+++ client/src/components/Home/HomePage.js
@@ -26,7 +26,7 @@ function HomePage(props) {
   return (
     <div className="homepage-desktop">
       <div className="banner-box position-relative">
-        <img alt='banner' className="home-desktop-banner" src={process.env.PUBLIC_URL + '/images/home/homepage-without-bottom.png'} />
+        <img onClick={() => {setCurrentHeaderLink('Products')}} alt='banner' className="home-desktop-banner" src={process.env.PUBLIC_URL + '/images/home/homepage-without-bottom.png'} />
       </div>
       <div className="homepage-banner-bottom">
         <div className="banner-text font-face-bebas">SCROLL TO DISCOVER OUR POPULAR PRODUCTS</div>
