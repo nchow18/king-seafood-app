041353657d57a92e68715ef1a50a47b0df044f5e
Nathan Chow <emailme@nathanchow.ca>
2021-12-05 18:58:01 -0500

    update total cart

--- client/src/components/Cart/PastOrders.js
+++ client/src/components/Cart/PastOrders.js
@@ -8,11 +8,14 @@ function PastOrders(props) {
     setPastOrderModal
   } = props
 
+  console.log(pastOrderCart);
+
   return (
     <div className="past-cart-container" onClick={() => {setPastOrderModal(false)}}>
       <div className="past-cart-details">
         <div><b>Message:</b> {pastOrderCart.message}</div><br></br>
         <div>Cart Items:</div>
+        <p>Cart Total: RM {pastOrderCart.user_cart_total}</p>        
         {pastOrderCart.cart.map((past) => (
           <div className="past-orders-item">
             <p>{past.product_name}</p>
