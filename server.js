cea5adb3dbfa2f4a63e751a2d523e1116bf2ed37
Nathan Chow <emailme@nathanchow.ca>
2022-04-11 16:11:45 -0400

    update

--- server.js
+++ server.js
@@ -20,14 +20,19 @@ const sess = {
 };
 
 const PORT = process.env.PORT || 3001;
-const corsOrigin = 'https://kingsseafood18.com';
+const corsOrigin = 'http://localhost:3000';
 
 app.use(session(sess));
 app.use(cors({
-  origin: [corsOrigin],
+  origin: true,
   methods: ['GET', 'POST', 'PUT'],
-  credentials: true
+  credentials: true,
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
